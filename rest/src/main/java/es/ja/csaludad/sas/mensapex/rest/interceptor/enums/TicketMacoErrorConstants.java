7a33a8c3046c1af26891d0808f88027b57490766
Sergio <silverlinder@gmail.com>
2026-03-18 19:07:30 +0100

    Mejoras solicitadas - Falta TEST

---
+++ rest/src/main/java/es/ja/csaludad/sas/mensapex/rest/interceptor/enums/TicketMacoErrorConstants.java
@@ -0,0 +1,12 @@
+package es.ja.csaludad.sas.mensapex.rest.interceptor.enums;
+
+public class TicketMacoError {
+    //constantes
+    public static final String MALFORMED_TICKET = "Ticket malformed";
+    public static final String EXPIRED_TICKET = "Ticket expired";
+    public static final String FUTURE_TICKET = "Future ticket received";
+    public static final String PERMISSION_TICKET = "Ticket Permission are not enough";
+    public static final String SIGNATURE_TICKET = "Ticket Signature invalid";
+
+    private TicketMacoError(){}
+}
